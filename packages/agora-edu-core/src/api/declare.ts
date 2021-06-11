829130a03098f1cfb41d781bf68f124db97ef027
matrixbirds <yooobuntu@gmail.com>
2021-06-12 01:43:57 +0800

    fix. na

--- packages/agora-edu-core/src/api/declare.ts
+++ packages/agora-edu-core/src/api/declare.ts
@@ -124,7 +124,7 @@ export const regionMap = {
   'AP': 'sg',
   'CN': 'cn-hz',
   'EU': 'gb-lon',
-  'NS': 'us-sv',
+  'NA': 'us-sv',
 } as const
 
 export type AgoraRegionString = 
--- packages/agora-edu-core/src/utilities/config.ts
+++ packages/agora-edu-core/src/utilities/config.ts
@@ -40,14 +40,14 @@ export const getRegion = (region: string) => {
     "CN": "GLOBAL",
     "AP": "ASIA",
     "EU": "EUROPE",
-    "NS": "NORTH_AMERICA",
+    "NA": "NORTH_AMERICA",
   }
 
   const rtmRegions: Record<string, string> = {
     "CN": "GLOBAL",
     "AP": "ASIA",
     "EU": "EUROPE",
-    "NS": "NORTH_AMERICA",
+    "NA": "NORTH_AMERICA",
   }
 
   return {
@@ -61,7 +61,7 @@ export const getRegionDomainCode = (region: string) => {
     "CN": "cn",
     "AP": "ap",
     "EU": "eu",
-    "NS": "na"
+    "NA": "na"
   }
 
   return regionDomain[region] ?? regionDomain["CN"]
