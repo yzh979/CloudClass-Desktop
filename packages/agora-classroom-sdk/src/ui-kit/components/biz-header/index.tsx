d080799779ac1e1b9ea4356a3bb3362b6b068e0b
zhong54 <836028215@qq.com>
2021-06-18 15:26:09 +0800

    fix bug

--- packages/agora-classroom-sdk/src/ui-kit/components/biz-header/index.tsx
+++ packages/agora-classroom-sdk/src/ui-kit/components/biz-header/index.tsx
@@ -112,7 +112,7 @@ export const BizHeader: FC<BizHeaderProps> = ({
         <div className="biz-header_md">
           { userType == 'teacher' &&
             <>
-              { classState == 'in-class' &&
+              { classState == 'pre-class' &&
                 <Button className="biz-header_md-join"  onClick={() => onClick('record')}>
                   <img src ={IconJoin}/>
                   <span>开始直播</span>
