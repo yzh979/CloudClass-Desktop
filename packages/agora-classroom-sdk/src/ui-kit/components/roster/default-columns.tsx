a24d7daea13c772d03b969a3b9070c0f1415e577
zhong54 <836028215@qq.com>
2021-06-22 22:22:37 +0800

    完成花名册

--- packages/agora-classroom-sdk/src/ui-kit/components/roster/default-columns.tsx
+++ packages/agora-classroom-sdk/src/ui-kit/components/roster/default-columns.tsx
@@ -3,10 +3,45 @@ import { Icon } from '~components/icon';
 import { Column, Profile } from '~components/roster';
 import {getCameraState, getMicrophoneState} from './base';
 
+import Star from './assets/star.png'
+import StarActive from './assets/star-active.png'
+
+import Covideo from './assets/covideo.png'
+import CovideoActive from './assets/covideo-active.png'
+
+import Pen from './assets/pen.png'
+import PenActive from './assets/pen-active.png'
+
+import Kick from './assets/kick.png'
+import KickActive from './assets/kick-active.png'
+
+import Camera from './assets/camera.png'
+import CameraDisable from './assets/camera-disable.png'
+
+import Microphone from './assets/microphone.png'
+import MicrophoneDisable from './assets/microphone-disable.png'
+
 export const defaultColumns: Column[] = [
   {
     key: 'name',
     name: '学员姓名',
+  },
+  {
+    key: 'stars',
+    name: '奖杯',
+    render: (text, profile: Profile, canOperate) => {
+      const operateStatus = !!canOperate === true ? 'operate-status' : 'un-operate-status';
+      const cls = classnames({
+        [`${operateStatus}`]: 1,
+      })
+      return (
+        <img src={profile.stars > 0 ? StarActive:Star}/>
+        // <span>
+        //   <Icon type={'star-outline'} className={cls} iconhover={canOperate}/>
+        //   <span className="star-nums">&nbsp;x{text}</span>
+        // </span>
+      )
+    },
   },
   {
     key: 'onPodium',
@@ -21,26 +56,10 @@ export const defaultColumns: Column[] = [
         [`${podiumStatus}`]: 1,
       })
       return (
-        <Icon type={type} className={cls} iconhover={canOperate}/>
+        // <Icon type={type} className={cls} iconhover={canOperate}/>
+        <img src={profile.onPodium ? CovideoActive:Covideo}/>
       );
     }
-  },
-  {
-    key: 'whiteboardGranted',
-    name: '授权画笔',
-    action: 'whiteboard',
-    render: (_, profile, canOperate) => {
-      const type =  !!profile.whiteboardGranted === true ? 'authorized' : 'whiteboard';
-      const operateStatus = !!canOperate === true ? 'operate-status' : 'un-operate-status';
-      const whiteboardStatus = !!profile.whiteboardGranted === true ? 'icon-active' : 'un-active';
-      const cls = classnames({
-        [`${operateStatus}`]: 1,
-        [`${whiteboardStatus}`]: 1,
-      })
-      return (
-        <Icon type={type} className={cls} iconhover={canOperate}/>
-      )
-    },
   },
   {
     key: 'cameraEnabled',
@@ -57,7 +76,8 @@ export const defaultColumns: Column[] = [
         [`${cameraStatus}`]: 1,
       })
       return (
-        <Icon type={type} className={cls} iconhover={canOperate}/>
+        // <Icon type={type} className={cls} iconhover={canOperate}/>
+        <img src={profile.cameraEnabled ? Camera:CameraDisable}/>
       )
     },
   },
@@ -76,26 +96,30 @@ export const defaultColumns: Column[] = [
         [`${microphoneStatus}`]: 1,
       })
       return (
-        <Icon type={type} className={cls} iconhover={canOperate}/>
+        // <Icon type={type} className={cls} iconhover={canOperate}/>
+        <img src={profile.micEnabled ? Microphone:MicrophoneDisable}/>
       )
     },
   },
   {
-    key: 'stars',
-    name: '奖杯',
-    render: (text, profile: Profile, canOperate) => {
+    key: 'whiteboardGranted',
+    name: '授权画笔',
+    action: 'whiteboard',
+    render: (_, profile, canOperate) => {
+      const type =  !!profile.whiteboardGranted === true ? 'authorized' : 'whiteboard';
       const operateStatus = !!canOperate === true ? 'operate-status' : 'un-operate-status';
+      const whiteboardStatus = !!profile.whiteboardGranted === true ? 'icon-active' : 'un-active';
       const cls = classnames({
         [`${operateStatus}`]: 1,
+        [`${whiteboardStatus}`]: 1,
       })
       return (
-        <div>
-          <Icon type={'star-outline'} className={cls} iconhover={canOperate}/>
-          <span className="star-nums">&nbsp;x{text}</span>
-        </div>
+        // <Icon type={type} className={cls} iconhover={canOperate}/>
+        <img src={profile.whiteboardGranted ? PenActive:Pen}/>
       )
     },
   },
+ 
   {
     key: 'kickOut',
     name: '移出',
