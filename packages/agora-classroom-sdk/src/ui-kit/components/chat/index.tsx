38c6f71faf9bb17b3cfc2f440b3eb099ffeb5771
zhong54 <836028215@qq.com>
2021-07-26 12:58:17 +0800

    svae

--- packages/agora-classroom-sdk/src/ui-kit/components/chat/index.tsx
+++ packages/agora-classroom-sdk/src/ui-kit/components/chat/index.tsx
@@ -77,6 +77,7 @@ export const Chat: FC<ChatProps> = ({
 }) => {
 
   const handleFocus = () => {
+    console.log('>>>>>>handleFocus')
     if(collapse && onCollapse){// 已关闭情况下打开
       onCollapse(false)
     }
@@ -139,7 +140,7 @@ export const Chat: FC<ChatProps> = ({
 
   return (
     <div className={`chat-wrap ${className}`}>
-      <div className="chat-panel" style={{visibility: collapse ? 'hidden':'visible'}} ref={chatHistoryRef} onScroll={handleScroll}>
+      <div className="chat-panel" style={{display: collapse ? 'none':'inherit'}} ref={chatHistoryRef} onScroll={handleScroll}>
       {
         messages.map((msg, idx) => {
           return (
