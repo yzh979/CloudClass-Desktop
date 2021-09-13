fb59e8be514d412c2dcd9f89c5ece5b3ffe27ede
matrixbirds <yooobuntu@163.com>
2021-09-13 19:27:32 +0800

    fix: types

--- packages/agora-edu-core-demo/src/ui-kit/utilities/renderer-player/index.tsx
+++ packages/agora-edu-core-demo/src/ui-kit/utilities/renderer-player/index.tsx
@@ -1,4 +1,4 @@
-import type { IMediaRenderer } from 'agora-edu-core';
+import type { IMediaRenderer } from 'agora-rte-sdk';
 import classnames from 'classnames';
 import * as React from 'react';
 import { useRendererPlayer } from '~utilities/hooks';
