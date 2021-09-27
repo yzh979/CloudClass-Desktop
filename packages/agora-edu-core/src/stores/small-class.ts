6d68247f0fdcfc1ab836bfa27868c073cd875cb6
matrixbirds <yooobuntu@163.com>
2021-09-27 20:04:34 +0800

    fix: student rewards

--- packages/agora-edu-core/src/stores/small-class.ts
+++ packages/agora-edu-core/src/stores/small-class.ts
@@ -244,21 +244,17 @@ export class SmallClassStore {
 
   @computed
   get studentsMap() {
-    if (this.roomInfo.roomType === EduSceneType.SceneLarge) {
-      const map = {}
-      return this.sceneStore.userList.reduce((acc: any, user: EduUser) => {
-        acc[user.userUuid] = {
-          uid: user.userUuid,
-          name: user.userName,
-          role: user.role,
-          userProperties: user.userProperties
-        }
-        return acc;
-      }, map)
-    } else {
-      const studentsMap = get(this.roomStore, 'roomProperties.students', {})
-      return studentsMap
-    }
+    const map = {}
+    return this.sceneStore.userList.reduce((acc: any, user: EduUser) => {
+      acc[user.userUuid] = {
+        uid: user.userUuid,
+        userUuid: user.userUuid,
+        name: user.userName,
+        role: user.role,
+        userProperties: user.userProperties
+      }
+      return acc;
+    }, map)
   }
 
   @action.bound
