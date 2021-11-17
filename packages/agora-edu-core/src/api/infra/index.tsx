de4155adac4fe9b6cc45486e1d0677a705d395e7
renyifei <renyifei@agora.io>
2021-11-17 14:22:28 +0800

    fix: update version

--- packages/agora-edu-core/src/api/infra/index.tsx
+++ packages/agora-edu-core/src/api/infra/index.tsx
@@ -45,7 +45,7 @@ const getLiveRoomPath = (roomType: number) => {
 
 export class AgoraEduCoreSDK {
   static get version(): string {
-    return '1.1.5-rc.11';
+    return '1.1.5-rc.13';
   }
 
   static _debug: boolean = false;
